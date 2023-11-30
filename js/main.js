c1a21900ceb26c8483359678e69af9d71e1ea4c8
shenaoro <shenaorj@gmail.com>
2023-11-30 16:38:25 -0500

    pagina versión 1.0 responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
+ }
+ 
+
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY>0);
 
 })
+
+
